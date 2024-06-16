682393bc811d79291d3ac0472b576fc199ef33fa
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-06-16 20:17:02 +0200

    remove validation generation

--- examples/src/main/java/github/benslabbert/vertxdaggercodegen/example/Main.java
+++ examples/src/main/java/github/benslabbert/vertxdaggercodegen/example/Main.java
@@ -1,13 +1,7 @@
 /* Licensed under Apache-2.0 2024. */
 package github.benslabbert.vertxdaggercodegen.example;
 
-import github.benslabbert.vertxdaggercodegen.example.validation.RequestDto;
-import java.util.List;
-
 public class Main {
 
-  public static void main(String[] args) {
-    RequestDto requestDto = new RequestDto("", 1, "", List.of(), List.of("friend", ""));
-    System.err.println("RequestDto is valid: " + requestDto.isValid());
-  }
+  public static void main(String[] args) {}
 }
